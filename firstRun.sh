ba4ede08c0b7bf663ff68ab25b06e1404fd8d9c5
mwatkins0590 <mwatkins0590@gmail.com>
2024-04-05 22:44:55 -0500

    inital upload

--- firstRun.sh
+++ firstRun.sh
@@ -5,5 +5,9 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update
 sudo apt install -y docker-ce
 sudo usermod -aG docker $USER
-sudo apt-get install libxrender1 libxtst6 libxi6 x11-apps
+sudo apt-get install libxrender1 libxtst6 libxi6 x11-apps libsecret-1-0 gnome-keyring terminator
 sudo apt install xclip
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt install -f
+sudo apt update
