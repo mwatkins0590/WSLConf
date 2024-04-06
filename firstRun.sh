00c2674b80251e9c1bd6c8c2a822ef985af318aa
mwatkins0590 <mwatkins0590@gmail.com>
2024-04-05 21:32:48 -0500

    inital upload

---
+++ firstRun.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y docker-ce
+sudo usermod -aG docker $USER
+sudo apt-get install libxrender1 libxtst6 libxi6 x11-apps
+sudo apt install xclip
